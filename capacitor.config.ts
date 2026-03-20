2a7b10d081813bd713b0b4dc39be44cf206e0e4d
rmallam <rmallam@users.noreply.github.com>
2026-03-20 10:41:01 +0530

    Fix capacitor typings for Vercel deployment

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.nutrisync.app',
   appName: 'NutriSync',
   webDir: 'public',
-  bundledWebRuntime: false,
   server: {
     url: 'http://192.168.1.107:3000',
     cleartext: true
